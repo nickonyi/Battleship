d70d894a87fe4a267ff9d23fa4acc0a598da0f08
nickonyi <nickoluoch1130@gmail.com>
2023-09-11 16:31:15 +0300

    creating the HTML layout of the project

---
+++ src/modules/dom/header.js
@@ -0,0 +1,8 @@
+function createHeaderBox() {
+    const header = document.createElement('div');
+    header.id = 'header';
+    const title = document.createElement('h1');
+    title.textContent = 'Battleship';
+}
+
+export default createHeaderBox;
