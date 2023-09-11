b3234202372e72ff7fc1eb212fb5d93167bda946
nickonyi <nickoluoch1130@gmail.com>
2023-09-11 15:55:42 +0300

    Project setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import sum from "./modules/aiLogic";
-console.log('Hello Kijana');
-console.log(sum(2,3));
-console.log('Hello mkubwa');
-alert('hello');
-prompt('bigman');
+import './style.css'
--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,23 @@ const path = require('path');
 module.exports = {
     mode: "development",
     devServer: {
-      static: "./dist",
+        static: "./dist",
     },
-  entry: './src/index.js', // Entry point of your application
-  output: {
-    filename: 'main.js', // Output bundle filename
-    path: path.resolve(__dirname, 'dist')
-}
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
