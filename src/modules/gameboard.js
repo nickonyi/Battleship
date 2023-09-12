8884c49032bfc1a5297f8713de6fa6dfde08bef0
nickonyi <nickoluoch1130@gmail.com>
2023-09-12 09:49:24 +0300

    Adding gameboard module

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,9 +1,24 @@
-function gameBoard() {
+import Ship from './ship';
+import shipTypes from './shipTypes';
 
+
+function GameBoard() {
+    const board = createEmptyGameBoard();
 
     function createEmptyGameBoard() {
         let gameBoardArray = [];
+        for (let row = 0; row <= 9; row++) {
+            let rowArray = [];
+            for (let col = 0; col <= 9; col++) {
+                rowArray[col] = null;
+            }
+            gameBoardArray[row] = rowArray;
+        }
+        return gameBoardArray;
+    }
+    return {
+        board
     }
 }
 
-export default gameBoard;
+export default GameBoard;
