3627f33c36fa8ee36cf0483961530984d97bd354
nickonyi <nickoluoch1130@gmail.com>
2023-09-12 09:58:22 +0300

    Adding player module

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,5 +1,17 @@
-function Player() {
+import GameBoard from "./gameboard";
 
+function Player(playerName, playerNumber) {
+    let name = typeof playerName == 'string' ? playerName : 'battlebot';
+    const number = playerNumber;
+    const isAi = typeof playerName == 'string' ? false : true;
+    const gameBoard = GameBoard();
+
+    return {
+        name,
+        number,
+        isAi,
+        gameBoard
+    }
 }
 
 export default Player;
