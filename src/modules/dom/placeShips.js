167525c43c32b002ca8ee16de818a1c284480d25
nickonyi <nickoluoch1130@gmail.com>
2023-09-14 21:12:42 +0300

    working on drag event functionality

--- src/modules/dom/placeShips.js
+++ src/modules/dom/placeShips.js
@@ -3,10 +3,29 @@ import shipTypes from "../shipTypes";
 let player;
 let board;
 
+// This object holds the data for the drag methods to use
+// These properties are updated as the user drags and drops ships
+const dragData = {
+    shipObject: null,
+    shipElement: null,
+    offsetX: null,
+    offsetY: null,
+    rowDif: 0,
+    colDif: 0,
+    shipHomeContainer: null,
+    previousContainer: null,
+    previousCell: null,
+    currentCell: null,
+}
+
 function drawSetupBoard(setupPlayer, setupBoard) {
     player = setupPlayer;
     board = setupBoard;
+    const setUpCells = board.querySelectorAll('.cell');
 
+    setUpCells.forEach(cell => {
+        cell.addEventListener('dragenter', dragEnter);
+    });
     return setupBoard;
 }
 
@@ -54,6 +73,10 @@ function drawShip(ship) {
         shipCell.classList.add('setup-ship-cell');
         shipBox.append(shipCell);
     }
+    shipBox.draggable = true;
+    shipBox.dataset.alignment = 'horizontal';
+    shipBox.addEventListener('dragstart', dragStart);
+    shipBox.addEventListener('dragend', dragEnd);
 
     const shipName = document.createElement('p');
     if (ship.name === 'patrol') {
@@ -61,10 +84,105 @@ function drawShip(ship) {
     } else {
         shipName.textContent = ship.name;
     }
-    shipContainer.append(shipBox, shipName);
+    shipContainer.append(shipName, shipBox);
     return shipContainer;
 }
 
+//This function handles the drag event when a user interacts with a particular ship element and enters a game cell into the board
+function dragEnter(event, touchCell) {
+    //1.clean up- removes any visual effects from with the previous drag operation
+    dragLeave(event);
+    //2.Prevent default behaviour of the drag operation
+    event.preventDefault();
+    //3.Extract data from the currently draagged ship and store it in the type variable
+    const type = dragData.shipElement.id;
+    let row;
+    let col;
+    //4.Depending on weather the current event is touch move or not we calculate the row and col values
+    //adjusting the difference between the origanl touch point and the cell
+    if (event.type === 'touchmove') {
+        row = parseInt(touchCell.dataset.row) - parseInt(dragData.rowDif);
+        col = parseInt(touchCell.dataset.col) - parseInt(dragData.colDif);
+    } else {
+        row = parseInt(event.target.dataset.row) - parseInt(dragData.rowDif);
+        col = parseInt(event.target.dataset.col) - parseInt(dragData.colDif);
+    }
+    //5. check weather is valid to place the ship in the given position on the board
+    const shipSquares = player.gameBoard.checkValidPlacement(shipTypes[type].length, [row, col], dragData.shipElement.dataset.alignment);
+    //6.filter invalid squares
+    //7.For each valid square create a visual overlay
+}
+
+function dragLeave(event) {
+    const leftCells = document.querySelectorAll('.cell-drag-over');
+    leftCells.forEach(cell => {
+        cell.remove();
+    });
+}
+
+// When the user starts dragging a ship, we store its information in dragData
+function dragStart(event) {
+
+    if (event.type === "touchstart") {
+        dragData.shipElement = event.target.parentElement;
+        dragData.shipHomeContainer = document.querySelector(`#${event.target.parentElement.id}-home`);
+        dragData.previousContainer = event.target.parentElement.parentElement;
+    } else {
+        dragData.shipElement = event.target;
+        dragData.shipHomeContainer = document.querySelector(`#${event.target.id}-home`);
+        dragData.previousContainer = event.target.parentElement;
+    }
+    updateCellDiff(event);
+
+    if (dragData.shipElement.dataset.alignment === 'vertical') dragData.shipElement.classList.add('setup-ship-vertical');
+    // On dragStart, we store the ship back in its home container & style it to be a 'ghost'
+    // Use a setTimeout to ensure this happens only after the ship has been picked up
+    setTimeout(() => {
+        dragData.shipElement.classList.add('setup-ship-hide');
+        dragData.shipElement.classList.remove('setup-ship-dropped');
+        dragData.shipElement.classList.remove('setup-ship-vertical');
+        dragData.shipHomeContainer.append(dragData.shipElement);
+    }, 0);
+    // If the ship is already placed (i.e., it's parent is a cell), we remove the ship from the player's gameboard
+    if (dragData.previousContainer.classList.contains('cell')) {
+        const cell = dragData.previousContainer;
+        const row = parseInt(cell.dataset.row);
+        const col = parseInt(cell.dataset.col);
+
+        player.gameBoard.removeShip([row, col]);
+    }
+}
+
+function dragEnd(event) {
+    dragData.shipElement.classList.remove('setup-ship-hide');
+}
+// When a user grabs a ship element, we track the user's cursor location for the dragEnter and drop events
+// When the ship is grabbed from the center, the cursor does not match up with the ship's origin cell
+// The cellDif difference between the origin cell to the cell where the user has grabbed the ship element
+// i.e., if a ship of length 5 is grabbed from the 4th cell, the cellDif will be 3
+function updateCellDiff(event) {
+    let x;
+    let y;
+    if (event.type === 'touchstart') {
+        let bcr = event.target.parentElement.getBoundingClientRect();
+        x = event.targetTouches[0].clientX - bcr.x;
+        y = event.targetTouches[0].clientY - bcr.y;
+        dragData.offsetX = x;
+        dragData.offsetY = y;
+    } else {
+        x = event.offsetX;
+        y = event.offsetY;
+    };
+
+    const cellSize = document.querySelector('.setup-ship-cell').offsetWidth;
+    if (dragData.shipElement.dataset.alignment === 'horizontal') {
+        dragData.rowDif = 0;
+        dragData.colDif = Math.floor(x / (cellSize + 2));
+    } else {
+        dragData.rowDif = Math.floor(y / (cellSize + 2));
+        dragData.colDif = 0;
+    }
+}
 const setup = {
     drawSetupBoard,
     drawSetupShips
--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -4,6 +4,7 @@ import shipTypes from './shipTypes';
 
 function GameBoard() {
     const board = createEmptyGameBoard();
+    const placedShips = [];
 
     function createEmptyGameBoard() {
         let gameBoardArray = [];
@@ -16,8 +17,44 @@ function GameBoard() {
         }
         return gameBoardArray;
     }
+
+    function checkSquare(row, col) {
+        if (row < 0 || col < 0) return undefined;
+        if (row > 9 || col > 9) return undefined;
+        else return board[row][col];
+    }
+
+    //check weather is valid to place a ship of a given length on a gameboard at a specified origin with a specifed alignmnent 
+    function checkValidPlacement(shipLength, origin, alignment) {
+        //1. create an array to store the coordinates of squares where the ship will be placed
+        //2. Destructure the origin array into row and col variables
+        //3. Use a for loop to iterate the whole ship length to calculate the cordinates of the squares the ship
+        //will ocuppy based on the alignment
+        //4. for each iterations add the [row,col] coordinates to the ship square array 
+        //and increment either the row or col based on the allignment
+        //5. After determining the squares the ship will occupy check the validity of the placemen
+
+        let [row, col] = origin;
+        let shipSquares = []
+    }
+
+    function removeShip(origin) {
+        const [row, col] = origin;
+        const ship = this.checkSquare(row, col);
+        ship.squares.forEach(square => {
+            const [row, col] = square;
+            this.board[row][col] = null;
+        });
+        const placedShipIndex = this.placedShips.indexOf(ship);
+        this.placedShips.splice(placedShipIndex, 1);
+    }
+
+
+
+
     return {
-        board
+        board,
+        removeShip
     }
 }
 
